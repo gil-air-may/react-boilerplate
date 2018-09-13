d675b1dc7b9a006e8e59e67d7e17da9704221fe4
GuilhermeGervasio <guilherme.gervasio@ateliware.com.br>
2018-09-13 10:41:02 -0300

    Prevent default was the solution I needed

--- src/Actions.js
+++ src/Actions.js
@@ -1,21 +1,29 @@
-import axios from 'axios'
+//import axios from 'axios'
 
-export function signinUser (email, password) {
+// export function signinUser (email, password) {
 
-  console.log({ email, password })
-    return function (dispatch) {
-      axios.post(`https://jsonplaceholder.typicode.com/posts`, { title: email, body: password, userId: '1' })
-      .then(res => {
-        //console.log('INSIDE THEN')
-        //console.log(res)
-        dispatch({type: 'AUTH_USER'})
-      })
-      .catch(err => {
-        console.log(err)
-        dispatch({type: 'Bad login info'});
-      });
-      
-  // return function(dispatch) {
-  // 
+//   //console.log({ email, password })
+//     return function (dispatch) {
+//       axios.post(`https://jsonplaceholder.typicode.com/posts`, { title: email, body: password, userId: '1' })
+//       .then(res => {
+//         //console.log('INSIDE THEN')
+//         //console.log(res)
+//         dispatch({type: 'AUTH_USER'})
+//       })
+//       .catch(err => {
+//         console.log(err)
+//         dispatch({type: 'Bad login info'});
+//       });
+
+//   // return function(dispatch) {
+//   //
+//   }
+// }
+
+export function signinUser() {
+
+  console.log('oi momaaa')
+  return function (dispatch) {
+    dispatch({ type: 'AUTH_USER' });
   }
 }
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 import * as actions from './Actions'
 
@@ -15,54 +16,96 @@ class Login extends React.Component {
     password: "",
   }
   
-  handleFormSubmit = async (event) => {
-    
-    // try {
-    //   await
-      this.props.signinUser(this.state.email, this.state.password);
-      console.log('execution is here')
-      if (this.props.isAuthenticated) 
-      {this.props.history.push("/private")}
-    
-    // } catch (error) {
-    //   alert(error.message)
-    // }
+ handleFormSubmit(e) {
+
+    e.preventDefault();
+    axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+    axios.post('/posts', {
+      title: 'yoo',
+      body: 'oyoo',
+      userId: '1'
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  
   }
 
   render() {
 
     return (
-      <form>
-        <div className="field">
-          <label className="label">Email</label>
-          <div className="control">
-            <input
-            className="input"
-            type="email"
-            placeholder="Your email address"
-            value={this.state.email}
-            autoFocus={true}
-            onChange={(e) => this.setState({...this.state, email: e.target.value})}
-            />
-          </div>
-      </div>
-      <div className="field">
-        <label className="label">Password</label>
-        <div className="control">
-          <input
-          className="input"
-          type="password"
-          placeholder="Your password"
-          value={this.state.password}
-          onChange={(e) => this.setState({...this.state, password: e.target.value})}
-          />
+      <div className="container" style={{ marginTop: '50px', width: '700px'}}>
+            <h2 style={{marginBottom: '40px'}}>Login</h2>
+            <form onSubmit={ (e) => this.handleFormSubmit(e)}>
+                <div className="form-group">
+                    <input
+                    type="email"
+                    placeholder="Email"
+                    //className={classnames('form-control form-control-lg', {
+                    //    'is-invalid': errors.email
+                   // })}
+                    name="email"
+                    onChange={ (e) => this.setState({...this.state, email: e.target.value}) }
+                    value={ this.state.email }
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                    type="password"
+                    placeholder="Password"
+                  //  className={classnames('form-control form-control-lg', {
+                   //     'is-invalid': errors.password
+                    //})} 
+                    name="password"
+                    onChange={ (e) => this.setState({...this.state, password: e.target.value}) }
+                    value={ this.state.password }
+                    />
+                </div>
+                <div className="form-group">
+                    <button>
+                        Login User
+                    </button>
+                </div>
+            </form>
         </div>
-      </div>
-      <button
-      className="button is-primary"
-      onClick={this.handleFormSubmit}
-      >Sign in</button>
-      </form>
+      
+      // <div>
+      //   <div className="field">
+      //     <label className="label">Email</label>
+      //     <div className="control">
+      //       <input
+      //       className="input"
+      //       type="email"
+      //       placeholder="Your email address"
+      //       value={this.state.email}
+      //       autoFocus={true}
+      //       onChange={(e) => this.setState({...this.state, email: e.target.value})}
+      //       />
+      //     </div>
+      // </div>
+      // <div className="field">
+      //   <label className="label">Password</label>
+      //   <div className="control">
+      //     <input
+      //     className="input"
+      //     type="password"
+      //     placeholder="Your password"
+      //     value={this.state.password}
+      //     onChange={(e) => this.setState({...this.state, password: e.target.value})}
+      //     />
+      //   </div>
+      // </div>
+      // <button
+      // type="submit"
+      // className="button is-primary"
+      // onSubmit={this.handleFormSubmit}
+      // onClick={this.handleFormSubmit}
+      // >Sign in</button>
+      // </div>
+        
       );
     }
 }
